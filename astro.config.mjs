08bbac4a41c4a3ccf9ab4ff5213065787b37a677
rohanvashisht <rohanprogrammer1@gmail.com>
2024-09-22 20:04:18 +0530

    ok

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,11 @@ import react from "@astrojs/react";
 
 import tailwind from "@astrojs/tailwind";
 
+import netlify from "@astrojs/netlify";
+
 // https://astro.build/config
 export default defineConfig({
   output:'server',
   integrations: [react(), tailwind()],
+  adapter: netlify(),
 });
